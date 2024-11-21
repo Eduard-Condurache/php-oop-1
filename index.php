1b0846e83b88c9179589781a1de2087b1449ca92
Eduard Condurache <eduardcondurache96@gmail.com>
2024-11-21 11:35:20 +0100

    test3

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ $interstellar->setAgeRestriction(16);
                 <tr>
                     <th scope="col">Title</th>
                     <th scope="col">Genere</th>
-                    <th scope="col">Trama</th>
+                    <th scope="col">Trama!</th>
                     <th scope="col">Lingua!</th>
                     <th scope="col">Durata</th>
                     <th scope="col">Età consentita</th>
